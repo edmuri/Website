91b16227b425902b2d63159d63c4d11192314103
Eddie <ed.murillo717@gmail.com>
2025-08-06 17:10:09 -0500

    changing pfp

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -4,8 +4,8 @@ import Description from "./Components/Description";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/HomePage.css";
-// import Pfp from "./photos/rainbow_self_photo.jpg";
-import Pfp from "./photos/funDog.jpg";
+import Pfp from "./photos/rainbow_self_photo.jpg";
+// import Pfp from "./photos/funDog.jpg";
 
 const HomePage = () =>{
     return (
