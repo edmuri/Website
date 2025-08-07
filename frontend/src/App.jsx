e2712340f91271903bd381a2549487368cca6bb6
Eddie <ed.murillo717@gmail.com>
2025-08-06 19:29:25 -0500

    editing App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Gallery from "./Pages/GalleryPage";
 import About from "./Pages/AboutPage";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter,BrowserRouter,Routes, Route} from "react-router-dom";
+import { HashRouter as BrowserRouter,Routes, Route} from "react-router-dom";
 
 function App(){
     return(
