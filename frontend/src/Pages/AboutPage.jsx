fc83e8ecb51082d6a4e1cb296ec5a5b9ab895ac5
Eddie <ed.murillo717@gmail.com>
2025-08-11 21:14:54 -0500

    fixing the favorites section

--- frontend/src/Pages/AboutPage.jsx
+++ frontend/src/Pages/AboutPage.jsx
@@ -14,7 +14,7 @@ const About = () => {
             <div className="topSection">
                 <AboutDesc/>
                 <div className="right-container">
-                    <img className="img-container" src={Pic}/>
+                    <div className="img-container"><img src={Pic}/></div>
                     <div className="fav-title-container">
                         <h1>My favorite songs</h1>
                         <div className="favoritesContainer">
