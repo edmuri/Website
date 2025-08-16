7df2ba525085d0765fdefc247d15d9c2feba07a3
Eddie <ed.murillo717@gmail.com>
2025-08-16 16:20:55 -0500

    fixing spelling error

--- frontend/src/Pages/Components/Description.jsx
+++ frontend/src/Pages/Components/Description.jsx
@@ -23,12 +23,12 @@ const Description = () => {
                     filters and create playlists for them. Our team is currently expanding to create more options for users.
                 </p>
                 <p className="Text">
-                    SOMNUS: A full stack project that allows users to share their dreams upon awaking with other 
+                    SOMNOS: A full stack project that allows users to share their dreams upon awaking with other 
                     users to create an interactive experience to relating to one another and getting interspective on the meanings
                     for your nights!
                 </p>
                 <p className="Text">
-                    NAME TBA: A new horror game that depicts a man discovering the existence of creatures existing in his local state park!
+                    KAOS: A new horror game that depicts a man discovering the existence of creatures existing in his local state park!
                 </p>
                 <p className="Text">
                     Take a look around my page! For more info about the projects above and previous projects, head to my projects tab. 
